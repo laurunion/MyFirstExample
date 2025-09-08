062557145136474487d7ee3411bd67b8db51f651
Lauren <owner@Ren.local>
2025-09-08 11:28:42 -0700

    add display at beginning, #62

--- main.cpp
+++ main.cpp
@@ -12,13 +12,13 @@ int main()
     int x,y;
 
     cin >> x >> y;
-    cout << "Addition: " << x + y << endl;
-    cout << "Subtraction: " << x - y << endl;
-    cout << "Multiplication: " << x * y << endl;
-    cout << "Division: " << x / y << endl;
-    cout << "Remainder: " << x % y << endl;
-    cout << "Square Root: " << sqrt(x) << endl;
-    cout << "Square: " << pow(x, y) << endl;
+    cout << "Addition: " << x << "+" << y << "=" << x + y << endl;
+    cout << "Subtraction: "<< x << "-" << y << "=" << x - y << endl;
+    cout << "Multiplication: " << x << "*" << y << "=" << x * y << endl;
+    cout << "Division: " << x << "/" << y << "=" << x / y << endl;
+    cout << "Remainder: " << x << "%" << y << "=" << x % y << endl;
+    cout << "Square Root: " << x << "√" << y << "=" << sqrt(x) << endl;
+    cout << "Square: " << x << "^2" << y << "=" << pow(x, y) << endl;
 
     return 0;
 }
