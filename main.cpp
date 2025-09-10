0924efee0159d8cf1fbf1c3282c9500dd36978f3
Lauren <owner@Ren.local>
2025-09-10 16:16:15 -0700

    x04: Resolve merge conflict in main.cpp (Closes #62)

--- main.cpp
+++ main.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
+using namespace std;
 #include <cmath>
 
 int main()
 {
-    std::cout << "THE FIRST EXAMPLE MATH DISPLAY!\n";
-    std::cout << "Hi, please enter two whole numbers: ";
+    cout << "THE FIRST EXAMPLE MATH DISPLAY!\n";
+    cout << "Hi, please enter two whole numbers: ";
 
     int x,y;
 
-    std::cin >> x >> y;
-    std::cout << "Addition: " << x + y << std::endl;
-    std::cout << "Subtraction: " << x - y << std::endl;
-    std::cout << "Multiplication: " << x * y << std::endl;
-    std::cout << "Division: " << x / y << std::endl;
-    std::cout << "Remainder: " << x % y << std::endl;
-    std::cout << "Square Root: " << sqrt(x) << std::endl;
-    std::cout << "Square: " << pow(x, y) << std::endl;
-
+    cin >> x >> y;
+    cout << "Addition: " << x << "+" << y << "=" << x + y << endl;
+    cout << "Subtraction: "<< x << "-" << y << "=" << x - y << endl;
+    cout << "Multiplication: " << x << "*" << y << "=" << x * y << endl;
+    cout << "Division: " << x << "/" << y << "=" << x / y << endl;
+    cout << "Remainder: " << x << "%" << y << "=" << x % y << endl;
+    cout << "Square Root: " << "√" << x << "=" << sqrt(x) << endl;
+    cout << "Square: " << x << "^" << y << "=" << pow(x, y) << endl;
     return 0;
 }
